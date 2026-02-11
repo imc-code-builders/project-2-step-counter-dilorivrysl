5460b5c2186a9d69392538c7a0f0a7b25a2c5a19
Demilade <demigodz821t@gmail.com>
2026-02-10 18:10:26 -0600

    finished starter

--- microbit_starter.py
+++ microbit_starter.py
@@ -4,10 +4,14 @@ import log
 log.set_labels('strength')
 while True:
     # Use log.add() to record the accelerometer strength
+    strength = accelerometer.get_strength()
     # You can get the strength with accelerometer.get_strength()
     # You must pass a dictionary to log.add
     # with the key as the name of the data ('strength') and the value as the actual strength
+    dict = {"strength": strength} 
 
-
+    # pass dict to log.add
+    log.add(dict)
+    
 
     sleep(100)
