d11fc23a9142a840ce14c6162b067051a6c16cf8
Demilade <demigodz821t@gmail.com>
2026-02-17 18:14:03 -0600

    finished project 1

--- project_work.py
+++ project_work.py
@@ -18,7 +18,8 @@ def get_changes(magnitudes):
     changes = []
 
     # TODO: For each pair, calculate: next_value - current_value
-    for i in range(len(magnitudes)):
+    for i in range(len(magnitudes) - 1):
+        changes.append(magnitudes[i + 1] - magnitudes[i])
         print(magnitudes[i])
     return changes
 
@@ -38,6 +39,9 @@ def count_peaks(changes, threshold):
     step_count = 0
 
     # TODO: Count changes greater than or equal to threshold
+    for change in changes:
+        if change > threshold:
+         step_count += 1
 
     return step_count
 
@@ -54,8 +58,13 @@ def count_steps(magnitudes, threshold=STEP_THRESHOLD):
     """
 
     # TODO: Use get_changes() and count_peaks()
+    
 
-    return 0
+
+    changes = get_changes(magnitudes,)
+    steps = count_peaks(changes, threshold)
+    return steps
+
 
 
 if __name__ == "__main__":
